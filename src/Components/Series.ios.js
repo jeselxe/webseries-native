0e7a7c793fb2dfc03f2c3113a03040309667a316
Jesús Gallego Irles <jesusgallego@outlook.com>
2015-12-29 00:09:43 +0100

    Add Seasons

--- src/Components/Series.ios.js
+++ src/Components/Series.ios.js
@@ -1,7 +1,9 @@
+/*global fetch*/
 import React, {PropTypes} from 'react-native';
 import {connect} from 'react-redux/native';
 import TableView from 'react-native-tableview';
 import Spinner from 'react-native-spinkit';
+import config from '../config';
 
 import Serie from './Serie.ios';
 
@@ -11,8 +13,41 @@ const {
     Alert,
 } = React;
 
+const mapStateToProps = (state) => {
+    return {
+        token: state.login.token,
+    };
+};
+
 const mapDispatchToProps = (dispatch) => {
     return {
+        addSeason: (token, serie) => {
+            const URL = `${config.api.url}/series/${serie}/temporada`;
+            return fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+        },
+        getSerie: (id) => {
+            const api = config.api.url;
+            fetch(`${api}/series/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                //Alert.alert('getSerie', JSON.stringify(data.serie.temporadas));
+                dispatch({
+                    type: 'SELECT_SERIE',
+                    serie: data.serie,
+                });
+                dispatch({
+                    type: 'GET_SEASONS',
+                    seasons: data.serie.temporadas,
+                });
+            })
+            .done();
+        },
         openModal: (title, component) => {
             dispatch({
                 type: 'OPEN_MODAL',
@@ -26,14 +61,30 @@ const mapDispatchToProps = (dispatch) => {
 class Series extends React.Component {
 
     static propTypes = {
+        addSeason: PropTypes.func,
         data: PropTypes.arrayOf(PropTypes.shape({
             id: PropTypes.number,
             title: PropTypes.string,
             description: PropTypes.string,
         })),
+        getSerie: PropTypes.func,
         navigator: PropTypes.shape({
             push: PropTypes.func,
         }),
+        token: PropTypes.string,
+    }
+
+    onNewSeason(serie) {
+        console.log(this.props);
+        if (this.props.token){
+            this.props.addSeason(this.props.token, serie)
+            .then(() => {
+                this.props.getSerie(serie);
+            });
+        }
+        else {
+            Alert.alert('No estás logueado','Inicia sesión primero');
+        }
     }
 
     render () {
@@ -58,11 +109,12 @@ class Series extends React.Component {
                                         onPress={() => this.props.navigator.push({
                                             title: serie.title,
                                             component: Serie,
+                                            onRightButtonPress: this.onNewSeason.bind(this, serie.id),
                                             passProps: {
                                                 serie,
                                             },
                                             rightButtonTitle: 'Nueva temporada',
-                                            onRightButtonPress: () => Alert.alert('Nueva temporada','SCRIPT'),
+
                                         })}
                                     >
                                         {serie.title}
@@ -87,4 +139,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default connect(null, mapDispatchToProps)(Series);
+export default connect(mapStateToProps, mapDispatchToProps)(Series);
